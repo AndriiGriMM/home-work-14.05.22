2a74ea11bacf1f96a91fd9bca0fbe7b8163c39c4
AndriiGriMM <webdevgrimm@gmail.com>
2022-05-15 16:36:56 +0300

    redone homework3

--- HM_4FREND_2/js/script.js
+++ HM_4FREND_2/js/script.js
@@ -1,12 +1,10 @@
 function equality() {
   let num = prompt("Введите число больше 100");
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 9; i++) {
     if (num < 100) {
       num = prompt("Вы ввели число меньше 100. введите снова");
-    } else {
-      console.log(num);
-      break;
-    }
+    } 
+    
   }
-  return console.log(num);
+  return console.log (num)
 }
